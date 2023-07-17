96b3422eda4f3f6d5594040c1267b796ecbdc715
DavidCDev <davidcastellanos.dev@gmail.com>
2023-07-17 17:55:26 -0500

    Modify functions to return promises and use async await

--- app.mjs
+++ app.mjs
@@ -2,28 +2,28 @@ import readlineSync from 'readline-sync';
 import chalk from 'chalk';
 import { addTask, deleteTask, completeTask } from './taskListFunctions.mjs';
 
-function selectOptions() {
+async function selectOptions() {
     const actions = ['Add a New Task', 'Complete a Task', 'Delete a task', 'Exit'];
     const index = readlineSync.keyInSelect(actions, 'What do you want to do?');
 
     switch (index) {
         case 0:
-            addTask(readlineSync.question('Enter a description for the task: '));
-            console.log(chalk.green('Task added successfully!'));
+            await addTask(readlineSync.question('Enter a description for the task: '))
+            console.log(chalk.green('Task added successfully!'))
             selectOptions();
             break;
         case 1:
-            completeTask(readlineSync.question('Enter the indicator of the task to complete: '));
+            await completeTask(readlineSync.question('Enter the indicator of the task to complete: '));
             console.log(chalk.green('Task completed successfully!'));
             selectOptions();
             break;
         case 2:
-            deleteTask(readlineSync.question('Enter the indicator of the task to delete: '));
+            await deleteTask(readlineSync.question('Enter the indicator of the task to delete: '));
             console.log(chalk.green('Task deleted successfully!'));
             selectOptions();
             break;
         case 3:
-            console.log(chalk.yellow('Exiting...'));
+            console.log(chalk.yellowBright('Exiting...'));
             break;
         default:
             console.log(chalk.red('Invalid option!'));
--- taskListFunctions.mjs
+++ taskListFunctions.mjs
@@ -1,25 +1,48 @@
+import chalk from 'chalk';
+
 let taskList = [];
 let indicator = 0;
 
 export const addTask = (description) => {
-    const task = {
-        indicator: indicator++,
-        description: description,
-        completed: false
-    }
+    return new Promise(resolve => {
+        const task = {
+            indicator: indicator++,
+            description: description,
+            completed: false
+        };
+        
+        taskList = [...taskList, task];
+        console.log(chalk.yellowBright('Adding task...'));
 
-    taskList = [...taskList, task];
+        setTimeout(() => {
+            resolve(task);
+        }, 2000);
+    });
 }
 
 export const deleteTask = (indicator) => {
-    let updatedTaskList = taskList.filter(task => task.indicator !== Number(indicator));
-    taskList = [...updatedTaskList];
+    return new Promise(resolve => {
+        let updatedTaskList = taskList.filter(task => task.indicator !== Number(indicator));
+        taskList = [...updatedTaskList];
+        console.log(chalk.yellowBright('Deleting task...'));
+
+        setTimeout(() => {
+            resolve();
+        }, 2000);
+    });
 };
   
 export const completeTask = (indicator) => {
-    taskList.forEach((task) => {
-        if (task.indicator === Number(indicator)) {
-            task.completed = true;
-        }
+    return new Promise(resolve => {
+        taskList.forEach((task) => {
+            if (task.indicator === Number(indicator)) {
+                task.completed = true;
+            }
+        });
+        console.log(chalk.yellowBright('Marking task as completed...'));
+        
+        setTimeout(() => {
+            resolve();
+        }, 2000);
     });
 };
